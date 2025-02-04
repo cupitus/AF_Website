c36af6d3112feb747b1fc32022356b128ec6d59c
cupitus <89416133+cupitus@users.noreply.github.com>
2025-02-04 13:41:10 +0000

    part 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+
+import { useState } from 'react'
+import { Background } from './Components/Background/Background/Background';
 
 export default function App() {
+let herodata = [
+  {text1 : "Dive into the world of", text2 : "American Football"},
+  {text1 : "Indulge", text2 : "your passion for the game"},
+  {text1 : "Get the latest Equipments", text2 : "Give in to your desires"},
+]
+
+const[heroCount, setheroCount] = useState(2);
+const[playStatus,setPlayStatus] = useState(false);
+
   return (
-    <div>App</div>
+    <div>
+      <Background playStatus={playStatus} heroCount = {heroCount} />
+    </div>
   )
 }
